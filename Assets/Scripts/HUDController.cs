335ddcb0210a1b5558ee8d7d12a42d7d2d6908cc
Chaz630 <36994814+driftingawaynow@users.noreply.github.com>
2024-10-15 21:22:57 -0500

    cursor

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -25,7 +25,6 @@ public class HUDController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void BlackBars(float size, float duration)
